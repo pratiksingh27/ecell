4e12eab773ffaccac588c1efa9229473f508f968
pratiksingh27 <94909677+pratiksingh27@users.noreply.github.com>
2022-01-02 15:45:47 +0530

    php

--- formhandler.php
+++ formhandler.php
@@ -4,14 +4,14 @@ $visitor_email= $_POST['email'];
 $subject= $_POST['subject'];
 $message= $_POST['message'];
 
-$email_from = '';
+$email_from = 'pratik27march@gmail.com';
 $email_subject = 'New form submission';
 $email_body = "User name: $name.\n".
             "User Email: $visitor_email.\n".
              "Subject: $subject.\n". 
              "User Message:$message.\n";
 
-$to ='pratik27march@gmail.com';
+$to ='pratik14uit@gmail.com';
 
 $headers ="from:$email_from \r\n";
 $headers .="Reply-To: $visitor_email \r\n;"
